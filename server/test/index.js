1ae28cbb46352eed063af4e9e1662c7f8b8d70bc
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-07 14:14:48 +0700

    create blog model

--- server/test/index.js
+++ server/test/index.js
@@ -1,1 +1,31 @@
+const app = require('../app')
+const mongoose = require('mongoose')
+const User = require('../models/user')
+
 const mocha = require('mocha')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const expect = chai.expect
+
+chai.use(chaiHttp)
+
+// describe("Test if local register works", function () {
+//
+//     afterEach(function () {
+//         chai.request(app)
+//             .delete('/api/user?username=admin')
+//     })
+//
+//     it("Return true if local register works", function () {
+//         chai.request(app)
+//             .post('/api/register')
+//             .field('name', 'admin')
+//             .field('username', 'admin')
+//             .field('password', 'testing')
+//             .field('email', 'admin@admin.com')
+//             .end(function (err, res) {
+//                 expect(res).to.have.status(200)
+//                 expect(res.name).to.be.equal('admin')
+//             })
+//     })
+// })
