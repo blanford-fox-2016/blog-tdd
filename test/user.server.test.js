ed8c0e5d1167d47c8c046e0dfbb2d255ff9f3cf1
broerjuang <juangwiantoro@gmail.com>
2016-11-07 14:54:09 +0700

    testing failed, callback isn't working

---
+++ test/user.server.test.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const mocha = require('mocha')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+
+const url = 'http://localhost:3000'
+
+
+describe('Nodejs Checking', function() {
+    it('Check the server', function(done) {
+      this.timeout(3000)
+      chai.request('http://localhost:3000')
+        .get('/')
+        .then(function(res) {
+          console.log('res');
+          expect(res).to.have.status(200)
+          done()
+        })
+    })
+    // it('Can post new user', () => {
+    //   chai.request(url)
+    //     .post('/api/users')
+    //     .send({
+    //       username : 'broerjuang',
+    //       name : 'juang wiantoro',
+    //       password : 'kambing'
+    //     })
+    //     .then(res => {
+    //       expect(res).to.have.status(200)
+    //       expect(res.body.username).to.equal('broerjuang')
+    //       console.log(res.body.);
+    //     })
+    // });
+  });
