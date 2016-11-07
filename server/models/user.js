ebbb0097212c35123dfc8dcdfe88e10f2a31ca69
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-07 12:53:21 +0700

    crete models user

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const User = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        validate: {
+            validator: function(email) {
+                return /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(email)
+            },
+            message: `format email salah`
+        },
+    },
+    post: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ]
+})
+
+module.exports = mongoose.model('User', User)
