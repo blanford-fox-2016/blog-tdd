faeb0faecf9813ce9b9ee523150e3ab64fa18a95
Ken Duigraha Putra <email": null>
2016-11-07 16:10:07 +0700

    refactor : testing api articles

--- server/test/api.articles.test.js
+++ server/test/api.articles.test.js
@@ -10,18 +10,18 @@ const url = 'http://localhost:3000'
   * should return (200) status code
   ** must be in format JSON
 */
-// describe('Get all articles from database', function(){
-//   it('it should return all articles from database', function(done){
-//     chai.request(url)
-//       .get('/api/articles')
-//       .end(function(err, res){
-//         console.log(res.body);
-//         res.should.be.json
-//         res.should.have.status(200)
-//         done()
-//       })
-//   })
-// })
+describe('Get all articles from database', function(){
+  it('it should return all articles from database', function(done){
+    chai.request(url)
+      .get('/api/articles')
+      .end(function(err, res){
+        console.log(res.body);
+        res.should.be.json
+        res.should.have.status(200)
+        done()
+      })
+  })
+})
 
 /*
   * will test POST /api/articles
@@ -29,21 +29,21 @@ const url = 'http://localhost:3000'
   ** must be in format JSON
   ** respond content body should be same with the content value that sent (POST)
 */
-// describe('Add a new article into database', function(){
-//   it('it should add new article', function(done){
-//     chai.request(url)
-//       .post('/api/articles')
-//       .send({
-//         "content" : "new content from testing"
-//       })
-//       .end(function(err, res){
-//         res.should.be.json
-//         res.should.have.status(200)
-//         res.body.content.should.equal("new content from testing")
-//         done()
-//       })
-//   })
-// })
+describe.skip('Add a new article into database', function(){
+  it('it should add new article', function(done){
+    chai.request(url)
+      .post('/api/articles')
+      .send({
+        "content" : "new content from testing"
+      })
+      .end(function(err, res){
+        res.should.be.json
+        res.should.have.status(200)
+        res.body.content.should.equal("new content from testing")
+        done()
+      })
+  })
+})
 
 /*
   * will test PUT /api/articles/:id
@@ -51,22 +51,22 @@ const url = 'http://localhost:3000'
   ** must be in format JSON
   ** respond content body should be same with the content value that sent (PUT)
 */
-// describe('Updated a specific article based on id', function(){
-//   let id = '58203f1fe46cbc11cd9d3d10'
-//   it('it should update a specific article', function(done){
-//     chai.request(url)
-//       .put('/api/articles/' + id)
-//       .send({
-//         "content" : "edit content from testing"
-//       })
-//       .end(function(err, res){
-//         res.should.be.json
-//         res.should.have.status(200)
-//         res.body.content.should.equal("edit content from testing")
-//         done()
-//       })
-//   })
-// })
+describe.skip('Updated a specific article based on id', function(){
+  let id = '582044a7ec55ae13c710d9a9'
+  it('it should update a specific article', function(done){
+    chai.request(url)
+      .put('/api/articles/' + id)
+      .send({
+        "content" : "edit content from testing"
+      })
+      .end(function(err, res){
+        res.should.be.json
+        res.should.have.status(200)
+        res.body.content.should.equal("edit content from testing")
+        done()
+      })
+  })
+})
 
 /*
   * will test DELETE /api/articles
@@ -74,16 +74,16 @@ const url = 'http://localhost:3000'
   ** must be in format JSON
   ** respond content body should be same with the content value that deleted (DELETE)
 */
-// describe('Deleted a specific article based on id', function(){
-//   let id = '582038621c8d280f37ce7cf8'
-//   it('it should delete a specific article', function(done){
-//     chai.request(url)
-//       .delete('/api/articles/' + id)
-//       .end(function(err, res){
-//         res.should.be.json
-//         res.should.have.status(200)
-//         res.body._id.should.equal(id)
-//         done()
-//       })
-//   })
-// })
+describe.skip('Deleted a specific article based on id', function(){
+  let id = '582044a7ec55ae13c710d9a9'
+  it('it should delete a specific article', function(done){
+    chai.request(url)
+      .delete('/api/articles/' + id)
+      .end(function(err, res){
+        res.should.be.json
+        res.should.have.status(200)
+        res.body._id.should.equal(id)
+        done()
+      })
+  })
+})
