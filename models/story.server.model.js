a4624349cc504e7b08b9ad459189e287d0a864ae
broerjuang <juangwiantoro@gmail.com>
2016-11-07 13:53:01 +0700

    creating story model

---
+++ models/story.server.model.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const storySchema = new Schema ({
+  title : String,
+  main  : String,
+  _creator : {
+    type : String,
+    ref : User
+  }
+})
+
+var Story = mongoose.model('Story', storySchema);
+module.exports = Story;
