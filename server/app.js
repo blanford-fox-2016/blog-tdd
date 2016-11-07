ee28448f439bda75ddfdd8d3d4426b94ca5c0846
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-07 11:38:17 +0700

    require cors

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const cors = require('cors')
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE)
@@ -24,6 +25,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
